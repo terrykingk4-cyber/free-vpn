59fc1c117b26182d96ecdcd431caa9debd196141
terrykingk4-cyber <terrykingk4@gmail.com>
2025-11-26 20:34:08 +0330

    Update MainActivity.kt

--- V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
+++ V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.v2ray.ang.ui
 
+import android.content.Intent
+import android.net.VpnService
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.AppConfig.VPN
 import com.v2ray.ang.R
+import com.v2ray.ang.databinding.ActivityMainBinding
+import com.v2ray.ang.handler.MmkvManager
+import com.v2ray.ang.handler.V2RayServiceManager
+import com.v2ray.ang.viewmodel.MainViewModel
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    val mainViewModel: MainViewModel by viewModels()
+
+    private val requestVpnPermission =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                startV2Ray()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val fab = findViewById<FloatingActionButton>(R.id.fab)
-
-        fab.setOnClickListener {
-            // اینجا کدی که باید با فشردن دکمه اجرا بشه رو قرار بده
+        //  ************  FAB اصلی پروژه — بدون حتی یک تغییر  ************
+        binding.fab.setOnClickListener {
+            if (mainViewModel.isRunning.value == true) {
+                V2RayServiceManager.stopVService(this)
+            } else if ((MmkvManager.decodeSettingsString(AppConfig.PREF_MODE) ?: VPN) == VPN) {
+                val intent = VpnService.prepare(this)
+                if (intent == null) {
+                    startV2Ray()
+                } else {
+                    requestVpnPermission.launch(intent)
+                }
+            } else {
+                startV2Ray()
+            }
         }
+        // **************************************************************
+    }
+
+    private fun startV2Ray() {
+        V2RayServiceManager.startVService(this)
     }
 }
